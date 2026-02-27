f47dc909dcd4ab0f54b92b7506294bb5a500524c
Bart Wolleswinkel <b.wolleswinkel@tudelft.nl>
2026-02-27 17:27:12 +0100

    [skip tests] Added the wraps module, for appropriate wrapping of factory patterns

--- src/numpes/_config/__init__.py
+++ src/numpes/_config/__init__.py
@@ -6,5 +6,7 @@ global
     This module defines the global configuration settings for NumPES, including numerical tolerances, solver settings, and assignment/initialization behavior.
 multipledispatch
     This module defines the multiple dispatch functionality used in NumPES, allowing for function overloading based on argument types in the Polytope constructor.
+wraps
+    This module defines a decorator for wrapping functions, used in for instance factory patterns.
 
 """
