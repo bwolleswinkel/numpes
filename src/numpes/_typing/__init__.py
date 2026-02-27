c73b34fa6b409c9037185324b9d45c4e5537b50f
Bart Wolleswinkel <b.wolleswinkel@tudelft.nl>
2026-02-27 15:53:18 +0100

    [skip tests] Added the private directory `_typing`, and an empty __init__.py and ndarray.py script/module

---
+++ src/numpes/_typing/__init__.py
@@ -0,0 +1,8 @@
+"""The private typing sub-package handles type definitions and annotations used throughout NumPES. This mainly includes a type alias for NumPy arrays. Note that, unfortunately, this cannot be used for by static type checkers such as `mypy`
+
+Modules
+-------
+ndarray
+    A type alias for NumPy arrays, used for type annotations throughout the codebase.
+
+"""
