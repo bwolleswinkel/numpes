513a88b80a1030101c826cac418edb3664241afc
Bart Wolleswinkel <b.wolleswinkel@tudelft.nl>
2026-02-27 11:53:57 +0100

    [skip tests] Added Functions to the docstring of geometric.py

--- src/numpes/control/geometric.py
+++ src/numpes/control/geometric.py
@@ -1,1 +1,8 @@
-"""Provides geometric control algorithms"""
+"""Provides geometric control algorithms
+
+Functions
+---------
+max_cond_inv
+    Calculates the maximum controlled invariant Subspace for a given LTI system.
+
+"""
