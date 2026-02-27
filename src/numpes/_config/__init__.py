39f309b24346e44004ec7683748ef6e34bded5e2
Bart Wolleswinkel <b.wolleswinkel@tudelft.nl>
2026-02-27 18:49:07 +0100

    [skip tests] Added the `_internal` directory, where I moved all the internal stuff to

--- src/numpes/_config/__init__.py
+++ src/numpes/_config/__init__.py
@@ -1,12 +1,7 @@
-"""The private config sub-package handles global configuration settings as well as defining some functionality which is used in the internal structuring of the code.
-
+"""The private config sub-package handles global configuration settings.
 Modules
 -------
 global
     This module defines the global configuration settings for NumPES, including numerical tolerances, solver settings, and assignment/initialization behavior.
-multipledispatch
-    This module defines the multiple dispatch functionality used in NumPES, allowing for function overloading based on argument types in the Polytope constructor.
-wraps
-    This module defines a decorator for wrapping functions, used in for instance factory patterns.
 
 """
---
+++ src/numpes/_internal/__init__.py
@@ -0,0 +1,12 @@
+"""This module contains the internal sub-package, which contains modules that are used for the internal structuring of the code, but are not intended to be used by users.
+
+Modules
+-------
+exceptions
+    This module defines the custom exceptions used in NumPES.
+multipledispatch
+    This module defines the multiple dispatch functionality used in NumPES, allowing for function overloading based on argument types in the Polytope constructor.
+wraps
+    This module defines a decorator for wrapping functions, used in for instance factory patterns.
+
+"""
