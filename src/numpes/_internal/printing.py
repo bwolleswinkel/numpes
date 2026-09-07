Added doctest, and also fixed some doctest. Fixed a bug in _str_hrepr. Also added 'units' to 3d rotation matrix for simplicity. Also made Self typing consistent with inheretence. Fixed bug in `pad` where char was never used. Added fixture-like in conftest to import numpes as pes for all docstring tests.

src/numpes/_internal/printing.py
@@ -42,17 +42,16 @@ def sym_replace(arr: str, char: str = CFG.sym_char) -> str:
     Examples
     --------
     >>> Q = np.array([[ 1, -1],
-                      [-1,  1])
+    ...               [-1,  1]])
     >>> print(sym_replace(str(Q)))
-    [[ 1, * ],
-     [-1,  1])
+    [[ 1  *]
+     [-1  1]]
 
     Truncation is also supported.
-     
     >>> A = np.arange(10_000).reshape(100, 100) * 1E-2
     >>> Q = A + A.T
     >>> with np.printoptions(precision=2, edgeitems=2):
-    >>>     print(sym_replace(str(Q), char='•'))
+    ...     print(sym_replace(str(Q), char='•'))
     [[  0.     •    ...   •      •   ]
      [  1.01   2.02 ...   •      •   ]
      ...
@@ -121,8 +120,8 @@ def format_as_set(elements: list[str], edgeitems: Optional[int] = None) -> str:
     Examples
     --------
     >>> verts = np.array([[1, 0],
-                          [0, 1],
-                          [0, 0]])
+    ...                   [0, 1],
+    ...                   [0, 0]])
     >>> print(format_as_set([str(np.atleast_2d(vert).T) for vert in verts]))
     /[[1]  [[0]  [[0] \\
     \\ [0]], [1]], [0]]/
@@ -187,7 +186,7 @@ def format_as_set(elements: list[str], edgeitems: Optional[int] = None) -> str:
 # [untested/unverified]
 def pad(text: str, length: int, char: str = " ") -> str:
     """Pad a string that is shorter than a certain length by appending copies of `char` (to the end).
-    Leaves the string unchanged if the string is of greater or equal lenght.
+    Leaves the string unchanged if the string is of greater or equal length.
     
     Parameters
     ----------
@@ -206,25 +205,25 @@ def pad(text: str, length: int, char: str = " ") -> str:
     Examples
     --------
     >>> text = "Sample text"
-    print(pad(text, 20, char="_"))
+    >>> print(pad(text, 20, char="_"))
     Sample text_________
 
     By default, a space is used as a padding character.
 
     >>> text_1, text_2 = "First part", "Second part"
-    print(pad(text_1, 30) + text_2)
+    >>> print(pad(text_1, 30) + text_2)
     First part                    Second part
 
     If the input text is longer then `length`, the original string is returned.
     >>> text = "Once upon a time"
-    print(pad(text, 5, char= "?"))
+    >>> print(pad(text, 5, char= "?"))
     Once upon a time
     """
     if len(char) != 1:
-        raise ValueError(f"The length of `char` must be 1, recieved '{char}' of length {len(char)}")
+        raise ValueError(f"The length of `char` must be 1, received '{char}' of length {len(char)}")
     return (text
             if len(text) >= length
-            else text + ''.join([' '] * (length - len(text))))
+            else text + ''.join([char] * (length - len(text))))
 
 
 def repr_items(obj: object,

src/numpes/polytope.py
@@ -82,7 +82,7 @@ class Polytope:
                  A_eq: Optional[ArrayLike] = None,
                  b_eq: Optional[ArrayLike] = None,
                  ) -> None:
-        """Initialize a Polytope from vertices or half-spaces.
+        r"""Initialize a Polytope from vertices or half-spaces.
 
         Parameters
         ----------
@@ -125,22 +125,34 @@ class Polytope:
         Examples
         --------
         Initialize a polytope from vertices (V-representation):
-        >>> verts = np.array([[0, 0], [1, 0], [0, 1]])
+        >>> verts = [[0, 0],
+        ...          [1, 0],
+        ...          [0, 1]]
         >>> poly = pes.poly(verts)
         >>> print(poly)
-        Polytope with 3 vertices in R^2
+        Polytope in R^2
+             /[[0]  [[1]  [[0] \
+        conv \ [0]], [0]], [1]]/
 
         Initialize a polytope from half-spaces (H-representation):
-        >>> A = np.array([[1, 0], [0, 1], [-1, 0], [0, -1]])
-        >>> b = np.array([1, 1, 0, 0])
+        >>> A = [[1, 0],
+        ...      [0, 1],
+        ...      [-1, 0],
+        ...      [0, -1]]
+        >>> b = [1, 1, 0, 0]
         >>> poly = pes.poly(A, b)
         >>> print(poly)
-        Polytope defined by 4 half-spaces in R^2
+        Polytope in R^2
+        [[ 1  0]  |    [[1]
+         [ 0  1]  |     [1]
+         [-1  0]  x <=  [0]
+         [ 0 -1]] |     [0]]
 
         Initialize an empty polytope in R^2:
         >>> poly = pes.poly(n=2)
         >>> print(poly)
-        Empty polytope in R^2
+        Polytope in R^2
+        [0 0] x <= [-1]
         """
         self._vrepr: tuple[NDArray, NDArray] | None = None
         self._hrepr: tuple[NDArray, NDArray] | None = None
@@ -787,7 +799,7 @@ class Polytope:
         --------
         >>> poly = pes.poly_from_point([1, 2, 3])
         >>> print(poly)
-        Singleton polytope in R^3
+        Polytope in R^3
         [[1. 0. 0.]  |    [[1.]
          [0. 1. 0.]  x ==  [2.]
          [0. 0. 1.]] |     [3.]]
@@ -932,7 +944,7 @@ class Polytope:
             A_lines = [pad(line, len(A_as_str[-1])) for line in A_as_str]
             x_lines = [" |    " if idx != idx_text else " x <= " for idx in range(nlines)]
             if b.dtype == float:  # Add array of zeros to avoid `-0.` in print output
-                b_lines = str(np.atleast_2d(b).T + np.zeros((1, self.n))).splitlines()
+                b_lines = str(np.atleast_2d(b).T + np.zeros((b.size, 1))).splitlines()
             else:
                 b_lines = str(np.atleast_2d(b).T).splitlines()
             comb_Ab = "\n".join(["".join(line) for line in zip(A_lines, x_lines, b_lines)])
@@ -948,7 +960,7 @@ class Polytope:
             A_eq_lines = [pad(line, len(A_eq_as_str[-1])) for line in A_eq_as_str]
             x_eq_lines = [" |    " if idx != idx_text_eq else " x == " for idx in range(nlines_eq)]
             if b_eq.dtype == float:  # Add array of zeros to avoid `-0.` in print output
-                b_eq_lines = str(np.atleast_2d(b_eq).T + np.zeros((1, self.n))).splitlines()
+                b_eq_lines = str(np.atleast_2d(b_eq).T + np.zeros((b_eq.size, 1))).splitlines()
             else:
                 b_eq_lines = str(np.atleast_2d(b_eq).T).splitlines()
             comb_Ab_eq = "\n".join(["".join(line) for line in zip(A_eq_lines, x_eq_lines, b_eq_lines)])
@@ -1072,7 +1084,7 @@ class Polytope:
                 M: NDArray,
                 calc_chebcr: bool = False,
                 in_place: bool = True,
-                ) -> Polytope | Self:
+                ) -> Self:
         """Matrix multiplication with a matrix `M`.
         
         Parameters
@@ -1139,7 +1151,7 @@ class Polytope:
     def minimal(self,
                 which_repr: Literal['both', 'vrepr', 'hrepr'] = 'both',
                 in_place: bool = True,
-                ) -> Polytope | Self:
+                ) -> Self:
         """Return a minimal representation of the polytope by removing redundant vertices and facets"""
         obj = self if in_place else self.copy()
         if which_repr in {'vrepr', 'both'}:

src/numpes/utils/linalg.py
@@ -142,6 +142,7 @@ def rot_mat_2d(angle: float) -> NDArray:
 # [untested/unverified]
 def rot_mat_3d(angles: list[float],
                convention: str | Literal['givens', 'yaw_pitch_roll'] = 'givens',
+               units: Literal['rad', 'deg'] = 'rad',
                ) -> NDArray:
     # FIXME: Instead of using 'proper_euler' and 'tait_bryan', I should use the much more clear 'xyz', 'XYZ', etc., for intrinsic and ectrinsit rotation, and just keep 'givens' and 'yaw_pitch_roll' as special cases.
     """Create a 3D rotation matrix from a sequence of angles based on the specified convention.
@@ -154,6 +155,8 @@ def rot_mat_3d(angles: list[float],
         The axis-order convention to use for constructing the rotation matrix (see also notes). Must be 3 characters belonging to the set {'X', 'Y', 'Z'} (for intrinsic rotations) or {'x', 'y', 'z'} (for extrinsic rotations). Extrinsic and intrinsic rotations cannot be mixed teh character sequence. Two special cases are provided for convenience:
         - 'yaw_pitch_roll': Yaw-Pitch-Roll angles (ZYX intrinsic). Identical to 'ZYX'.
         - 'givens': QR-like adjacent-plane Givens sweep (xzx extrinsic). Identical to 'xzx'.
+    units : 'rad' or 'deg', default='rad'
+        The units of the provided angles
 
     Returns
     -------
@@ -162,30 +165,25 @@ def rot_mat_3d(angles: list[float],
 
     Examples
     --------
-    >>> angles = np.deg2rad([0, -45, 90])
-    >>> print(pes.utils.rot_mat_3d(angles, convention='yaw_pitch_roll').round(2))
-    [[ 0.    1.    0.  ]
+    >>> angles = [0, -45, 90]
+    >>> print(pes.utils.rot_mat_3d(angles, convention='yaw_pitch_roll', units='deg').round(2))
+    [[ 0.71 -0.   -0.71]
      [-0.71  0.   -0.71]
-     [-0.71  0.    0.71]]
-    >>> print(pes.utils.rot_mat_3d(angles, convention='xzx').round(2))
-    [[ 0.71  0.   -0.71]
-     [ 0.71  0.    0.71]
-     [ 0.   -1.    0.  ]]
-
-    Use `pes.utils.angles_3d_convert` to convert between different conventions of 3D rotation angles.
-
-    >>> angles = np.deg2rad([-30, 60, 45])  # Proper Euler angles in YZY convention
-    >>> angles_converted = pes.utils.angles_3d_convert(angles, from_convention='yzy', to_convention='givens')
-    >>> print(pes.utils.rot_mat_3d(angles_converted))
-    [[ 0.66 -0.75  0.05]
-     [ 0.61  0.5  -0.61]
-     [ 0.44  0.43  0.79]]
+     [ 0.    1.    0.  ]]
+    >>> print(pes.utils.rot_mat_3d(angles, convention='xzx', units='deg').round(2))
+    [[ 0.71  0.71  0.  ]
+     [ 0.    0.   -1.  ]
+     [-0.71  0.71  0.  ]]
 
     Notes
     -----
     Note that extrinsic rotations are equivalent to intrinsic rotations in the reverse order. For example, a zyx extrinsic rotation is equivalent to an XYZ intrinsic rotation.
     """
-    angles = angles_3d_convert(angles, from_convention=convention, to_convention='givens')
+    if units not in {'rad', 'deg'}:
+        raise ValueError(f"Units must be either 'rad' or 'deg', received unknown option '{units}'")
+    angles = angles_3d_convert(angles if units == 'rad' else np.deg2rad(angles).tolist(),
+                               from_convention=convention,
+                               to_convention='givens')
     return rot_mat(angles)
 
 